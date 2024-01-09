ead8a8c13b97b0f5259ebf46096d01538392dbff
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-01-09 14:00:14 -0300

    aula 1 e 2 do curso

---
+++ aula001/aula01.js
@@ -0,0 +1,9 @@
+console.log("Olá Mundo")
+console.log("Tudo bem!!!")
+
+
+//Os comentários
+/*
+console.log("Olá Mundo")
+console.log("Olá Mundo")
+*/
---
+++ aula002/aula02.js
@@ -0,0 +1,6 @@
+"use strict" //Modo restrito / estrito
+
+let nome = "Bruno"
+console.log(nome)
+console.log('Olá, seja bem vindo!')
+console.log('Nome: ' + nome) 
