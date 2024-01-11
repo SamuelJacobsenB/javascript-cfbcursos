3f426ef931153230ab64a3b2b62de5dc882d8d33
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-01-11 14:49:55 -0300

    aula09

--- aula009/aula09.js
+++ aula009/aula09.js
@@ -1,5 +1,17 @@
 let n = 10
 
+let x = -n
 
+console.log(x)
+
+console.log('-----------------------') 
+//-----------------------------------------------------
+console.log(n++)
+
+console.log(n)
+
+console.log('-----------------------')
+
+console.log(++n)
 
 console.log(n)
