455f93c0fb6315f5532730b1a115a6de3daf209c
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-01-17 10:22:39 -0300

    Atualizando aulas

---
+++ aula016/aula16.js
@@ -0,0 +1,24 @@
+const objs = document.getElementsByTagName("div")
+
+let num = [10,20,30,40,50]
+
+//for in
+for(o in objs){
+    console.log(num[n])
+}
+
+console.log('-----------------')
+
+//for of
+for(o of objs){
+    console.log(n)
+}
+
+console.log('-----------------')
+
+//normal
+//for(let i = 0; i < num.length;i++){
+ //   console.log(num[i])
+//}
+
+//veja no browser
---
+++ aula018/aula18.js
@@ -0,0 +1,15 @@
+let n = 10
+
+do {
+    console.log('Curso JS')
+    n++
+} while(n < 10)
+
+
+while(n < 10){
+    console.log('Curso JS')
+    n++
+}
+
+
+console.log('Fim do Programa')
