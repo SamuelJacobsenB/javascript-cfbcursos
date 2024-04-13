8efcde75d1fb218673585ece7c8f0c8f948f0e51
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-04-13 14:12:13 -0300

    Aula 101

---
+++ aula101/aula101.js
@@ -0,0 +1,16 @@
+let nome = new String('Samuel Jacobsen Barcellos')
+let canal = new String('CFBCursos')
+
+console.log(nome)
+console.log(typeof(nome))
+
+console.log(nome.charAt(0))
+console.log(nome.charAt(1))
+
+console.log(nome.charCodeAt(0))
+
+console.log(nome.concat(canal))
+
+console.log(nome.indexOf('a'))
+
+console.log(nome.lastIndexOf('s'))
