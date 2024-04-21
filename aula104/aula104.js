c41cb5c9c237cf8007a9443348b539033376393f
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-04-21 09:05:58 -0300

    Aula 104

---
+++ aula104/aula104.js
@@ -0,0 +1,9 @@
+let nome = new String('Samuel Jacobsen Barcellos')
+
+console.log(nome.startsWith('S'))
+
+console.log(nome.endsWith('Barcellos'))
+
+console.log(nome.includes('s'))
+
+console.log(nome.repeat(2))
