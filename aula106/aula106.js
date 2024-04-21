a4c2a2ddebaa13337771d62b63618c52424936ef
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-04-21 17:32:52 -0300

    Aula 106

--- aula106/aula106.js
+++ aula106/aula106.js
@@ -1,5 +1,6 @@
 let nome = new String('Samuel Jacobsen Barcellos 2010')
 let email = 'samuel@samuel.com.br'
+let numeros = '1, 10, 100, 1000'
 console.log(nome)
 
 console.log(nome.search(/samuel/i))
@@ -10,4 +11,17 @@ console.log(nome.replace(/s/ig,'$'))
 
 console.log(nome.match(/[a-c|g-l|0-1]/ig))
 
-console.log(nome.match(/\d/ig))
+//Metacaracteres
+console.log(nome.match(/\d/ig)) //Números
+console.log(nome.match(/\s/ig)) //Espaços
+console.log(nome.match(/\bS/ig)) //DWORD
+//
+
+//Quantificadores
+let nome2 = new String('Samueeeeeel Jacobseeeeen Barcelloooooos 2010')
+console.log(numeros)
+console.log(nome2.match(/e+|o+/ig))
+console.log(numeros.match(/10/ig))
+console.log(numeros.match(/10+/ig))
+console.log(numeros.match(/10*/ig))
+console.log(numeros.match(/10?/ig))
